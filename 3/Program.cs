8da443d17647d9ff0ac77da02dc7525b79f0653b
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-10 15:59:42 +0300

    working project

--- 3/Program.cs
+++ 3/Program.cs
@@ -6,7 +6,42 @@ namespace _3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+                Console.WriteLine("enter your number a ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter your number b ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter your number c ");
+                int c = Convert.ToInt32(Console.ReadLine());
+
+                if (a > 0 & b > 0 & c > 0 )
+                {
+                    Console.WriteLine("Your result is " + (a + b + c));
+                }
+                else if (a < 0 & b > 0 & c > 0 )
+                {
+                    Console.WriteLine("Your result is " + (b + c));
+                }
+                else if (a > 0 & b < 0 & c > 0 )
+                {
+                    Console.WriteLine("Your result is " + (a + c));
+                }
+                else if (a > 0 & b > 0 & c < 0)
+                {
+                    Console.Write("Your result is  " + (a + b));
+                }
+            else if (a > 0 & b < 0 & c < 0)
+            {
+                Console.Write("Your result is  " + a);
+            }
+            else if (a < 0 & b > 0 & c < 0)
+            {
+                Console.Write("Your result is  " + b);
+            }
+            else if (a < 0 & b < 0 & c > 0)
+            {
+                Console.Write("Your result is  " + c);
+            }
         }
     }
-}
+    }
